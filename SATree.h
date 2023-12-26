95d0cc4ec4121959b5204c528914ba41057206c5
Tanay Tummalapalli <ttanay100@gmail.com>
2023-12-27 01:32:28 +0530

    cosmetics

--- SATree.h
+++ SATree.h
@@ -1,25 +1,32 @@
-#include <vector>
 #include <string>
+#include <vector>
 
-struct Point { int x; int y; };
+struct Point
+{
+    int x;
+    int y;
+};
 using Points = std::vector<Point>;
 
-struct SATreeNode {
+struct SATreeNode
+{
     Point point;
     std::vector<SATreeNode *> neighbours;
     float covering_radius{0};
 
-    SATreeNode(Point p): point(p) {}
+    SATreeNode(Point p) : point(p) { }
     ~SATreeNode();
 };
 
-class SATree {
-    void build(SATreeNode *a, std::vector<Point> &S);
-    void print(SATreeNode *node, int space);
-    std::string to_string(SATreeNode *node);
-public:
-    // TODO: Make private
+class SATree
+{
     SATreeNode * root;
+
+    void build(SATreeNode * a, std::vector<Point> & S);
+    void print(SATreeNode * node, int space);
+    std::string to_string(SATreeNode * node);
+
+public:
     SATree(std::vector<Point> & S);
     ~SATree();
 
--- SATree_test.cpp
+++ SATree_test.cpp
@@ -1,30 +1,31 @@
-#include "SATree.h"
 #include <iostream>
 #include <string>
 
-enum TestCaseStatus { PASS, FAIL };
+#include "SATree.h"
+
+enum TestCaseStatus
+{
+    PASS,
+    FAIL
+};
 struct TestCaseResult
 {
     std::string name;
     TestCaseStatus status;
     std::string err;
 
-    void display() {
-        if(status == TestCaseStatus::PASS)
+    void display()
+    {
+        if (status == TestCaseStatus::PASS)
             std::cout << "TestCase \"" << name << "\": PASSED" << std::endl;
         else
             std::cout << "TestCase \"" << name << "\": FAILED with" << err << std::endl;
     }
 };
 
-TestCaseResult test_case1(const std::string &name)
+TestCaseResult test_case1(const std::string & name)
 {
-    Points input {
-        {3, 3},
-        {5, 3},
-        {2, 2},
-        {4, 4}
-    };
+    Points input{{3, 3}, {5, 3}, {2, 2}, {4, 4}};
     SATree tree(input);
     std::string res = tree.to_string();
     /**
@@ -34,22 +35,18 @@ TestCaseResult test_case1(const std::string &name)
             |- (5, 3) |N(a)| = 0
     */
     std::string expected = "{4, 4}({3, 3}({2, 2}),{5, 3})";
-    if(res == expected)
+    if (res == expected)
         return {name, TestCaseStatus::PASS, ""};
-    else {
+    else
+    {
         auto err = "Assert failed!\n Expected: " + expected + "; Got: " + res;
         return {name, TestCaseStatus::FAIL, err};
     }
 }
 
-TestCaseResult test_case2(const std::string &name) {
-    Points input {
-        {-3, -3},
-        {-3, -4},
-        {-5, -3},
-        {-4, -4},
-        {-1, -1}
-    };
+TestCaseResult test_case2(const std::string & name)
+{
+    Points input{{-3, -3}, {-3, -4}, {-5, -3}, {-4, -4}, {-1, -1}};
     SATree tree(input);
     auto res = tree.to_string();
     // Note: the {-5,-3} should be a child of {-3,-3} because it is the
@@ -63,15 +60,16 @@ TestCaseResult test_case2(const std::string &name) {
 
     */
     std::string expected = "{-1, -1}({-3, -3}({-3, -4}({-4, -4}),{-5, -3}))";
-    if(res == expected)
+    if (res == expected)
         return {name, TestCaseStatus::PASS, ""};
-    else {
+    else
+    {
         auto err = "Assert failed!\n Expected: " + expected + "; Got: " + res;
         return {name, TestCaseStatus::FAIL, err};
     }
 }
 
-int main(int argc, char **argv)
+int main(int argc, char ** argv)
 {
     test_case1("Basic tree construction").display();
     test_case2("Skipped neighbour").display();
